115727c636927f29189d530052b2814d5ac3f684
prime-infinity <soe.a.prime@gmail.com>
2021-11-14 12:17:06 +0100

    sending responses to requests

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 //without framework
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req,res)=> { //this callback function is going to run for every request
-    console.log("request made", req)
+    console.log(req.url, req.method)
+
+    //set headers
+    res.setHeader('content-Type','text/plain')
+
+    res.write('hello');
+    res.write('write another');
+    
+    //send an html file
+    /*fs.readFile('./views/index.html', (err,data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        else{
+            res.write(data);
+            res.end()
+        }
+    })*/
+
+
+    res.end()
 })
 server.listen(3000, 'localhost',()=>{ //127.0.0.1
     console.log("listening for request on port 3000")
-})
+})  
