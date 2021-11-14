b5725c21440abb8425c45210bca7f7d4942f09db
prime-infinity <soe.a.prime@gmail.com>
2021-11-14 12:36:27 +0100

    redirects,statusCodes

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 //without framework
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path/posix');
 
 const server = http.createServer((req,res)=> { //this callback function is going to run for every request
     console.log(req.url, req.method)
 
     //set headers
     res.setHeader('content-Type','text/plain')
+
+    //routing
+    switch(req.url){
+        case '/':
+            //send index data
+            //res.statusCode = 200;
+            break;
+        case '/about':
+            //send about data
+            //res.statusCode = 200;
+            break;
+        case 'about-me': //redirecting
+            /*res.statusCode = 301;
+            res.setHeader('Location','/about')
+            res.end()*/
+            break;
+        default:
+            //send default data
+            //res.statusCode = 400;
+            break;
+    }
 
     res.write('hello');
     res.write('write another');
