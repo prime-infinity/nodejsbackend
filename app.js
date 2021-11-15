a17f992f98316c9081efa8d00f4eb20b42cf5402
prime-infinity <soe.a.prime@gmail.com>
2021-11-15 21:52:59 +0100

    in middleware

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express()
 app.listen(3000)
 
 app.get('/', (req,res) => {
-
-    res.send("hel")
-
+    res.send("home")
+    //res.sendFile('./views/index.html',{ root: __dirname })
 });
+
+app.get('/about', (req,res) => {
+    res.send("about")
+});
+
+//404 page
+app.use((req,res) => { //this opsition is important
+    //res.status(404).sendFile('./views/404.html', {root:__dirname})
+})
